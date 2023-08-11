8022455ca2d242bc18ef4bb9ff66a2c83fbdd5ca
Giacomo <giacomo.madella.98@gmail.com>
2023-08-11 12:12:59 +0200

    test2_ready && postcall todo

--- TODO.md
+++ TODO.md
@@ -8,29 +8,49 @@
 - [x] Try shared auto shm_transport 
 - [ ] get-set function prototype
 - [ ] Realtime message (only qos?)
-- [ ] CHeck tcp, udp, shmem
 - [ ] Dynamic types  
 - [ ] [Progetto di ricerca](./research_project/TODO.md)
 
 # August
-Baically 4 scheme
+Baically 4 things
 - [ ] Google-doc skeleton of entities and protagonist envolved in HPC communication with type, request etc.
   - [x] TESTcpp create a 4 entities scheme (sender - 1 pub 1 sub on callback; receiver 1sub 1 pub for callback)
 - [ ] Perform metric in which we record difference on atomic (shared_mem udp_multicas udp_unicast tcp?)
+  - [x] do custom dds_on specific implementation
+    - [x] tcp (some problems on different nodes)
+    - [x] udp
+    - [x] upd-multicast
+    - [x] memshared
 - [ ] Is using wildcard partition costly?
-  - [ ] Design a test to prove that ( maybe wit changin partition dinamically in order to see if is increasing)
-    - [ ] 1 dummy for discovery
-    - [ ] 1 pubisher with dynamic wildcard partitions
-    - [ ] n subscriber with fixe interconnected partitions
+  - [x] Design a test to prove that ( maybe wit changin partition dinamically in order to see if is increasing)
+    - [x] 1 dummy for discovery
+    - [x] 1 pubisher with dynamic wildcard partitions
+    - [x] n subscriber with fixe interconnected partitions
+    - [x] An idea should be:
+      - [x] 1 (N1) pub 48(N2) sub all interconnected partition with udp (DUMMY)
+      - [x] 1 (N1) pub parition group* 48(N3) sub partition group0, then try to divide in multiple group the group0. For example 48/4 group0-3, then 48/8 group0-5 etc
+- [ ] Study the regale lib and implement your version
 - [ ] measure LOAD CORE - avg LATENCY per message- **THROUGHPUT** (maybe after?)
 
+---
+
+- [ ] dummy monitoring (federico)
+  - [ ] + articolato (servirebbe anche un broker mqtt dummy) bridge; 
+    - [ ] fastdds subscriba a tutti i topic e li manda al broker
+    - [ ] è necessario poter ricostruire le informazioni di chi invia le info (lato dds)
+  - [ ] guarda il derivarable
+- [ ] dummy jobmanager (francesco)
+
+
 ## minor
-- [ ] remove the first data (that contain discovery latency) from measured metrics
-- [ ] store data out in a file, not in output (and manage that)
-- [ ] chechk how to store the metrics isntead of printing on stdout?
+- [x] remove the first data (that contain discovery latency) from measured metrics
+- [x] store data out in a file, not in std-output (and manage that)
 - [x] check how to change partition at runtime (on dds_partitions)
 - [ ] same process qos ingore
-
+- [x] asyncronous-sincronous mode
+- [x] implement Federico's metrics
+- [ ] more than 10 messages for latency & THROUGHPUT
+- [ ] tcp on different galileo100's nodes
 
 # Ask to prof.
 ## Andrea
@@ -40,6 +60,13 @@ Baically 4 scheme
 ## Federico
 - [x] Va bene il branch atomic che ho fatto
 - [x] Ho accesso a Cineca, come fare per fare i test? -> call on _?_
+- [x] devo studiare la parte di julita e luis? (not now)
+  - [ ] list instead of uint32
+  - [ ] tcp
+  - [ ] asy-sync mode?
+  - [ ] get-set
+- [ ]improve atomic time empiricamente o idealmente?
+
 
 
 
---
+++ cineca/test_wildcards/src/sub.cpp
@@ -0,0 +1,298 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file HelloWorldSubscriber.cpp
+ *
+ */
+
+#include "HelloWorldPubSubTypes.h"
+
+
+#include <functional>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/rtps/transport/shared_mem/SharedMemTransportDescriptor.h>
+#include <fastdds/rtps/transport/UDPv4TransportDescriptor.h>
+#include <fastdds/rtps/transport/TCPv4TransportDescriptor.h>
+#include <fastrtps/utils/IPLocator.h>
+#include <ctime>
+#include <fstream>
+
+
+using namespace eprosima::fastdds::dds;
+using namespace eprosima::fastdds::rtps;
+using namespace eprosima::fastrtps::rtps;
+timespec received_time_;
+
+class HelloWorldSubscriber
+{
+private:
+
+    DomainParticipant* participant_;
+
+    Subscriber* subscriber_;
+
+    DataReader* reader_;
+
+    Topic* topic_;
+
+    TypeSupport type_;
+
+    class SubListener : public DataReaderListener
+    {
+    public:
+
+        SubListener()
+            : samples_(0)
+        {
+        }
+
+        ~SubListener() override
+        {
+        }
+        void on_subscription_matched(
+                DataReader*,
+                const SubscriptionMatchedStatus& info) override
+        {
+            if (info.current_count_change == 1)
+            {
+                this->count+=1;
+            }
+            else if (info.current_count_change == -1)
+            {
+                this->count-=1;
+                std::cout << "Partecipant exited" << this->count <<std::endl;
+
+            }
+            if (this->count == 0){
+                this->pubPresent=false;
+                return;
+            }
+        }
+        
+        void on_data_available(
+                DataReader* reader) override
+        {
+            SampleInfo info;
+            if (reader->take_next_sample(&hello_, &info) == ReturnCode_t::RETCODE_OK)
+            {
+                if (info.valid_data)
+                {
+                    clock_gettime(CLOCK_MONOTONIC,&received_time_);
+                    received_vector_.push_back(received_time_);
+                    samples_++;
+                    std::cout << "Message: " << hello_.message() << " after time: " << received_time_.tv_nsec << " RECEIVED." << std::endl;
+                }
+            }
+        }
+        HelloWorld hello_;
+        std::atomic_int samples_;
+        bool pubPresent;
+        int count;
+        timespec received_time_;
+        std::vector<timespec> received_vector_; 
+
+    } listener_;
+
+public:
+
+    HelloWorldSubscriber()
+        : participant_(nullptr)
+        , subscriber_(nullptr)
+        , topic_(nullptr)
+        , reader_(nullptr)
+        , type_(new HelloWorldPubSubType())
+    {
+    }
+
+    virtual ~HelloWorldSubscriber()
+    {
+        if (reader_ != nullptr)
+        {
+            subscriber_->delete_datareader(reader_);
+        }
+        if (topic_ != nullptr)
+        {
+            participant_->delete_topic(topic_);
+        }
+        if (subscriber_ != nullptr)
+        {
+            participant_->delete_subscriber(subscriber_);
+        }
+        DomainParticipantFactory::get_instance()->delete_participant(participant_);
+    }
+
+    //!Initialize the subscriber
+    bool init(std::string transport,char * partition)
+    {
+        DomainParticipantQos participantQos;
+        participantQos.name("transport_custom");
+        participantQos.wire_protocol().builtin.discovery_config.leaseDuration = eprosima::fastrtps::c_TimeInfinite;
+
+        if ("tcp" == transport){
+            std::cout << "using tcp" << std::endl;
+            // Client: do not configure listening port!
+            // Set up tcp transport mode
+            participantQos.transport().use_builtin_transports = false;
+            std::shared_ptr<TCPv4TransportDescriptor> descriptor = std::make_shared<TCPv4TransportDescriptor>();
+            participantQos.transport().user_transports.push_back(descriptor);
+            // Set initial peers
+            Locator initial_peer_locator;
+            initial_peer_locator.kind = LOCATOR_KIND_TCPv4;
+            //IPLocator::setIPv4(initial_peer_locator, "80.80.99.45"); // Here in localhost we should use 127.0.0.1; different nodes: ip's node ex. 80.80.99.45. 
+            eprosima::fastrtps::rtps::IPLocator::setPhysicalPort(initial_peer_locator, 5100);
+            eprosima::fastrtps::rtps::IPLocator::setLogicalPort(initial_peer_locator, 5100);
+            participantQos.wire_protocol().builtin.initialPeersList.push_back(initial_peer_locator);
+
+        }
+        else if ("udpM" == transport)
+        {
+            std::cout << "using udp-multicast" << std::endl;
+        }
+        else if ("shm" == transport)
+        {
+            std::cout << "using sharedMemory" << std::endl;
+            participantQos.transport().use_builtin_transports = false;
+            auto sm_transport = std::make_shared<SharedMemTransportDescriptor>();
+            sm_transport->segment_size(2 * 1024 * 1024);
+            participantQos.transport().user_transports.push_back(sm_transport);
+        }
+        else {
+            std::cout << "using udp" << std::endl;
+        }
+
+        participant_ = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
+
+        if (participant_ == nullptr)
+        {
+            return false;
+        }
+
+
+        listener_.pubPresent=true;
+        listener_.count=0;
+
+        // Register the Type
+        type_.register_type(participant_);
+
+        // Create the subscriptions Topic
+        topic_ = participant_->create_topic("default", "HelloWorld", TOPIC_QOS_DEFAULT);
+
+        if (topic_ == nullptr)
+        {
+            return false;
+        }
+
+        // Create the Subscriber
+        SubscriberQos subscriberQos = SUBSCRIBER_QOS_DEFAULT;
+
+        subscriberQos.partition().push_back(partition);  //SET PARTITION PASSED IN argv
+       
+        subscriber_ = participant_->create_subscriber(subscriberQos, nullptr);
+
+        if (subscriber_ == nullptr)
+        {
+            return false;
+        }
+
+        auto dr_qos = DATAREADER_QOS_DEFAULT;
+        if ("udpM" == transport){
+            eprosima::fastrtps::rtps::Locator_t new_multicast_locator;
+            eprosima::fastrtps::rtps::IPLocator::setIPv4(new_multicast_locator, "239.255.0.4");
+            new_multicast_locator.port = 7900;
+            dr_qos.endpoint().multicast_locator_list.push_back(new_multicast_locator);
+        }
+        else if ("tcp" == transport)
+        {
+            //ONLY QOS setting, not really useful to tcp
+            // dr_qos.history().kind = eprosima::fastdds::dds::KEEP_LAST_HISTORY_QOS;
+            // dr_qos.history().depth = 30;
+            // dr_qos.resource_limits().max_samples = 50;
+            // dr_qos.resource_limits().allocated_samples = 20;
+            // dr_qos.reliability().kind = eprosima::fastdds::dds::RELIABLE_RELIABILITY_QOS;
+            // dr_qos.durability().kind = eprosima::fastdds::dds::TRANSIENT_LOCAL_DURABILITY_QOS;
+        }
+        
+        reader_ = subscriber_->create_datareader(topic_, dr_qos, &listener_);
+
+        if (reader_ == nullptr)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    //!Run the Subscriber
+    std::vector<timespec> run()
+    {
+        while(listener_.pubPresent)
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(100));
+        }
+        return listener_.received_vector_;
+    }
+};
+
+
+void printDataToFile(const std::string& filename, std::vector<timespec> vector_to_print ) {
+    std::ofstream outputFile(filename);
+    if (outputFile.is_open()) {
+        // Print the data structure elements to the file
+        for (const auto& element : vector_to_print) { // or dataDeque
+            outputFile  << "received_s: " << element.tv_sec << " received_ns: " <<  element.tv_nsec << std::endl;
+        }
+        outputFile.close();
+        std::cout << "Data has been written to the file: " << filename << std::endl;
+    } else {
+        std::cout << "Error opening the file: " << filename << std::endl;
+    }
+}
+
+
+int main(
+        int argc,
+        char** argv)
+{
+
+    if (argc < 4) {
+        std::cout << "USAGE: /$0 partition transport N" << std::endl;
+        return 1;
+    }
+    std::string inputString = argv[1];
+    std::string transport = argv[2];  
+    std::string write = argv[3]; 
+    char* partition = const_cast<char*>(inputString.c_str());
+    std::vector<timespec> save;
+    HelloWorldSubscriber* mysub = new HelloWorldSubscriber();
+    if(mysub->init(transport,partition))
+    {
+        save = mysub->run();
+    }
+    // for (const auto& element : save) {
+    //     std::cout << " received_ns: " <<  element.tv_nsec << std::endl;
+    // }
+    std::string filename= "sub_"+ transport + "_" + inputString + "_" + write +".data";
+    printDataToFile(filename,save);
+
+    delete mysub;
+    return 0;
+}
